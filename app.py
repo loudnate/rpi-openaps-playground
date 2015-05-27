6dcda460fc67018f12a905a66d61feb03e27f4bd
Nathan Racklyeft <loudnate@gmail.com>
2015-05-27 11:07:16 -0700

    Run script cleanup and comments

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def glucodyn_live():
+    """Renders a GlucoDyn effect graph from the current pump settings and recent history
+
+    Note that this invokes the pump RF calls on demand. Great for debugging, but not for normal use.
+    """
+    import json
     from subprocess import check_output
 
-    return check_output(["openaps", "use", "pump", "read_settings"])
+    read_settings_dict = json.loads(check_output(["openaps", "use", "pump", "read_settings"]))
+
+    return json.dumps(read_settings_dict)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
-docker build -t loudnate/naterade .
-docker run -i -t -p=80:5000 --device=`readlink -fn /dev/serial/by-id/usb-0a21_8001-if00-port0`:/dev/serial/by-id/usb-0a21_8001-if00-port0 -v=/dev/log:/dev/log loudnate/naterade
+#!/usr/bin/env bash
+
+# For development, mount the source directory using the -v argument
+# For production, use the build command
+
+# docker build -t loudnate/naterade .
+docker run -i -t \
+    -p=80:5000 \
+    --device=`readlink -fn /dev/serial/by-id/usb-0a21_8001-if00-port0`:/dev/serial/by-id/usb-0a21_8001-if00-port0 \
+    -v=/dev/log:/dev/log \
+    -v=`pwd`:/app \
+    loudnate/naterade
